06e6300e8739f11a3caf97de16fbbf7015722e76
lesk <les@yoohoomedia.com.au>
2019-02-10 13:51:09 +1100

    added self test button on tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,38 +1,49 @@
-import {Component, OnInit}      from '@angular/core';
-import {ActivatedRoute, Params} from '@angular/router';
+import {Component, OnInit}              from '@angular/core';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
-})
+               selector   : 'app-tab2',
+               templateUrl: 'tab2.page.html',
+               styleUrls  : ['tab2.page.scss']
+           })
 export class Tab2Page implements OnInit {
 
-  private query_param_results: Params;
-  private param_results: Params;
+    private query_param_results: Params;
+    private param_results: Params;
 
-  constructor(private route: ActivatedRoute) {
+    constructor(private route: ActivatedRoute, private router: Router) {
 
-  }
+    }
 
-  ngOnInit(): void {
-    console.log("Tab2Page ngOnInit()");
+    ngOnInit(): void {
+        console.log('Tab2Page ngOnInit()');
 
-    this.route.queryParams.subscribe(data => {
-      console.log('Tab2Page() QUERY PARAM: ', data);
-      this.query_param_results = data;
-    });
-    this.route.params.subscribe(data => {
-      console.log('Tab2Page() PARAM: ', data);
-      this.param_results = data;
-    });
-  }
+        this.route.queryParams.subscribe(data => {
+            console.log('Tab2Page() QUERY PARAM: ', data);
+            this.query_param_results = data;
+        });
+        this.route.params.subscribe(data => {
+            console.log('Tab2Page() PARAM: ', data);
+            this.param_results = data;
+        });
+    }
 
-  getQueryParam() {
-    return JSON.stringify(this.query_param_results && this.query_param_results);
-  }
+    getQueryParam() {
+        return JSON.stringify(this.query_param_results && this.query_param_results);
+    }
 
-  getParam() {
-    return JSON.stringify(this.param_results && this.param_results);
-  }
+    getParam() {
+        return JSON.stringify(this.param_results && this.param_results);
+    }
+
+    getRandomNumber() {
+        return Math.random();
+    }
+
+    navigateToSelf() {
+        this.router.navigate(['/tabs', 'tab2'],
+                             {
+                                 queryParams: {random: this.getRandomNumber()},
+                             });
+    }
 }
